4cea012f145654450f1e211ac7a929b264507907
Anibabz <habeebanibaba07@gmail.com>
2025-06-02 14:28:44 +0100

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from config import settings
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 if not SQLALCHEMY_DATABASE_URL:
