c9969fc0f24881e94ce492e88ab85a823711c972
Anibabz <habeebanibaba07@gmail.com>
2025-06-02 14:43:07 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-   DATABASE_URL: str
-   APP_NAME: str = "Full Stack To Do App"
+    DATABASE_URL: str
+    APP_NAME: str = "Full Stack To Do App"
 
     class Config:
         env_file = ".env"
         extra = "ignore"
 
+settings = Settings()
 
