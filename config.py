6ac9108ee99bcf0da5ac13ced718bdaed67430c4
Anibabz <habeebanibaba07@gmail.com>
2025-06-02 13:58:53 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,16 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-     DATABASE_HOST: str
-     DATABASE_NAME: str
-     DATABASE_USER: str
-     DATABASE_PASSWORD: str
-     DATABASE_PORT: int
-     app_name: str = "Full Stack To Do App"
+    DATABASE_HOST: str
+    DATABASE_NAME: str
+    DATABASE_USER: str
+    DATABASE_PASSWORD: str
+    DATABASE_PORT: int
+    APP_NAME: str = "Full Stack To Do App"
 
-     class Config:
-         env_file = ".env"
-         extra = "ignore"
+    class Config:
+        env_file = ".env"
+        extra = "ignore"
+
+# Singleton instance to be reused everywhere
+settings = Settings()
