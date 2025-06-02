2e99e88028960aab3f1d75bc9bcd31c2dde40c1f
Anibabz <habeebanibaba07@gmail.com>
2025-06-02 14:00:00 +0100

    Update database.py

--- database.py
+++ database.py
@@ -1,19 +1,13 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
+from config import settings
 
-load_dotenv()
 SQLALCHEMY_DATABASE_URL = (
-    f"postgresql://{os.environ['DATABASE_USER']}:{os.environ['DATABASE_PASSWORD']}"
-    f"@{os.environ['DATABASE_HOST']}:{os.environ['DATABASE_PORT']}/{os.environ['DATABASE_NAME']}"
+    f"postgresql://{settings.DATABASE_USER}:{settings.DATABASE_PASSWORD}"
+    f"@{settings.DATABASE_HOST}:{settings.DATABASE_PORT}/{settings.DATABASE_NAME}"
 )
 
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
- )
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
